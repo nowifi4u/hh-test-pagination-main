8d0a467a4fc8241ced7a4903d1fbfb9809fca9a2
Maksim Shamber <36640388+nowifi4u@users.noreply.github.com>
2024-05-07 15:35:02 +0300

    feat: add query parameter page for UserController.getAllUsers

--- srv/src/users/user.controller.ts
+++ srv/src/users/user.controller.ts
@@ -1,6 +1,7 @@
 import { UserService } from './users.service.js';
 import { Controller, Get, Logger } from '@nestjs/common';
 import { UsersResponseDto } from './users.response.dto.js';
+import { Query } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
@@ -8,9 +9,18 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
-  async getAllUsers() {
-    this.logger.log('Get all users');
-    const users = await this.userService.findAll();
-    return users.map((user) => UsersResponseDto.fromUsersEntity(user));
+  async getAllUsers(@Query('page') _page: string | undefined) {
+    let page = Number.parseInt(_page);
+    if (isNaN(page)) page = 1;
+    if (page < 1) page = 1;
+
+    this.logger.log(`/users?page=${page} (raw page=${_page})`);
+
+    const [users, count] = await this.userService.findPage(page);
+    return {
+      users: users.map((user) => UsersResponseDto.fromUsersEntity(user)),
+      count,
+      page,
+    };
   }
 }
--- srv/src/users/users.service.ts
+++ srv/src/users/users.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UsersEntity } from './users.entity.js';
 import { Repository } from 'typeorm';
 
+export const USERS_PER_PAGE = 20;
+
 @Injectable()
 export class UserService {
   private readonly logger = new Logger(UserService.name);
@@ -15,5 +17,13 @@ export class UserService {
   // get list of all users
   async findAll(): Promise<UsersEntity[]> {
     return await this.usersRepo.find();
+  }
+
+  async findPage(page: number = 1): Promise<[UsersEntity[], number]> {
+    if (page < 1) page = 1;
+    return await this.usersRepo.findAndCount({
+      skip: (page - 1) * USERS_PER_PAGE,
+      take: USERS_PER_PAGE,
+    });
   }
 }
